8055de87fbf566d078374ec4c2bc6b2923c28e1e
gunyu1019 <gunyu1019@gmail.com>
2020-08-07 14:14:53 +0900

    Add Post&form

---
+++ Form.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <form action="form_action.php" method="get">
+      <input type="text" name="title" placeholder="Title"><br/>
+      <br/>
+      <textarea name="description"></textarea><br/>
+      <br/>
+      <input placeholder="제출", type="submit">
+    </form>
+  </body>
+</html>
